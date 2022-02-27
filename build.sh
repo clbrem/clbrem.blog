ad4633d784a548ebfa039feecc257c80e077fba7
clbrem <cbremer4@gmail.com>
2022-02-27 15:02:54 -0600

    Updated build script

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 6.0 -InstallDir ./dotnet6
 ./dotnet6/dotnet --version
-./dotnet6/dotnet run --project ./Blog/Blog.fsproj -o /dist
+mkdir dist
+./dotnet6/dotnet run --project ./Blog/Blog.fsproj -o ../dist
