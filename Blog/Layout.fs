0b6c84c5ea73610c78e602191acf85f0c920403d
clbrem <cbremer4@gmail.com>
2022-03-06 16:08:14 -0600

    Made page responsive

--- Blog/Layout.fs
+++ Blog/Layout.fs
@@ -13,26 +13,33 @@ module Layout =
     let linkScript (scriptLink:string) (context: IExecutionContext) =
         let myLink = context.GetLink(scriptLink)
         script [ _src myLink ]     
-    let defaultHead (context: IExecutionContext) attrs content =        
+    let defaultHead (doc: IDocument, context: IExecutionContext) attrs content =
+        doc.
+        let index = context.OutputPages.["index.html"].FirstOrDefaultDestination()
         head [yield! attrs] [
-            linkStyle "/assets/styles.css" context
-            yield! content
+              meta [ _charset "utf-8" ] 
+              meta [_httpEquiv "X-UA-Compatible"; _content "IE=edge"]
+              meta [ _name "viewport"; _content "width=device-width, initial-scale=1, shrink-to-fit=no"]
+              meta [_name "author"; _content (index.Get<string>("Author"))]
+              meta [_name "description"; _content (index.Get<string>("Summary")) ]
+              linkStyle "/assets/styles.css" context
+              linkStyle "/assets/paper.css" context
+              yield! content
         ]
     let defaultBody attributes  =
         body [attr "data-menu" "true"; yield! attributes] 
-    let layout (context: IExecutionContext) =
+    let layout (doc: IDocument, context: IExecutionContext) =
         fun _header _body ->
             html [_lang "en"] [
-                yield defaultHead context [] _header
+                yield defaultHead (doc, context) [] _header
                 yield! _body
             ]
-    let layoutRaw headers body (doc: IDocument, context : IExecutionContext) =
-        
+    let layoutRaw headers body (doc: IDocument, context : IExecutionContext) =        
         task {
             use stream = doc.GetContentStream()
             use reader = new StreamReader(stream)
             let! output = reader.ReadToEndAsync()
-            return [yield HtmlElements.rawText output; yield! body] |> layout context headers
+            return [yield HtmlElements.rawText output; yield! body] |> layout (doc, context) headers
         }
         
     let prismHead =  
